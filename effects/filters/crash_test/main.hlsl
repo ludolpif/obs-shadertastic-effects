70fc7bcf759ee6ee5889cb7decab996f844212de
ludolpif <ludolpif@gmail.com>
2024-06-30 22:23:26 +0200

    crash_test: add a filter with a parse error in main.hlsl to investigate OBS segfaults

---
+++ effects/filters/crash_test/main.hlsl
@@ -0,0 +1,74 @@
+// Common parameters for all shaders, as reference. Do not uncomment this (but you can remove it safely).
+/*
+uniform float time;            // Time since the shader is running. Goes from 0 to 1 for transition effects; goes from 0 to infinity for filter effects
+uniform texture2d image;       // Texture of the source (filters only)
+uniform texture2d tex_interm;  // Intermediate texture where the previous step will be rendered (for multistep effects)
+uniform float upixel;          // Width of a pixel in the UV space
+uniform float vpixel;          // Height of a pixel in the UV space
+uniform float rand_seed;       // Seed for random functions
+uniform int current_step;      // index of current step (for multistep effects)
+*/
+// Specific parameters of the shader. They must be defined in the meta.json file next to this one.
+uniform float progression;
+
+// Additionnal definitions for this shader
+#define PI 3.1415926535
+//----------------------------------------------------------------------------------------------------------------------
+
+// These are required objects for the shader to work.
+// You don't need to change anything here, unless you know what you are doing
+sampler_state textureSampler {
+    Filter    = Linear;
+    AddressU  = Clamp;
+    AddressV  = Clamp;
+};
+
+struct VertData {
+    float2 uv  : TEXCOORD0;
+    float4 pos : POSITION;
+};
+
+struct FragData {
+    float2 uv  : TEXCOORD0;
+};
+
+VertData VSDefault(VertData v_in)
+{
+    VertData vert_out;
+    vert_out.uv  = v_in.uv;
+
+    // Mirror effect done via vert_out.pos.x modification
+    float half_width = 1.0/upixel/2.0;
+    float v_mirror_pos_x = (v_in.pos.x-half_width)*cos(progression*PI)+half_width ;
+    float4 v_miror = float4(v_mirror_pos_x, v_in.pos.y, v_in.pos.z, 1.0);
+
+    vert_out.pos = mul(v_miror, ViewProj);
+    return vert_out;
+}
+
+// This effect don't use pixel shader at all, it just tweaks the vertex shadeer
+float4 EffectLinear(float2 uv)
+{
+    float4 rgba = image.Sample(textureSampler, uv);
+/* CRASH TEST HERE */
+if ( true ) {
+/* CRASH TEST HERE */
+    return rgba;
+}
+
+// You probably don't want to change anything from this point.
+
+float4 PSEffect(FragData f_in) : TARGET
+{
+    float4 rgba = EffectLinear(f_in.uv);
+    return rgba;
+}
+
+technique Draw
+{
+    pass
+    {
+        vertex_shader = VSDefault(v_in);
+        pixel_shader = PSEffect(f_in);
+    }
+}
