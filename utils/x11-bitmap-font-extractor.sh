c2a5a67a420e624f7958c4d470e3527f0915faf4
ludolpif <ludolpif@gmail.com>
2024-06-30 13:23:16 +0200

    x11-bitmap-font-extractor.sh: add "." and "-", reverse bits to not have vertical mirror effect

--- utils/x11-bitmap-font-extractor.sh
+++ utils/x11-bitmap-font-extractor.sh
@@ -2,19 +2,26 @@
 zcat /usr/share/fonts/X11/misc/4x6.pcf.gz | pcf2bdf | grep -EA13 "^STARTCHAR (zero|one|two|three|four|five|six|seven|eight|nine)$" | awk --non-decimal-data \
 	'BEGIN {
        		digit=0; fontwidth=4; fontheight=6;
+		indent="        ";
+		print "#define font_width " fontwidth ".0"
+		print "#define font_height " fontheight ".0"
 	       	print "#define DigitBin(x) ( \\"
        	}
-	/^STARTCHAR/ { character=0; printf "x==" digit "?" }
+	/^STARTCHAR/ { character=0; printf indent "x==" digit "?" }
 	/^BITMAP/ { lineno=fontheight }
 	/^..$/ { if (lineno > 0 ) {
-       		line = rshift(sprintf("%d", "0x" $1), 8-fontwidth);
+		parsed = sprintf("%d", "0x" $1)
+		line = 0
+		for (i=0; i<8; i++) {
+			bit = and(rshift(parsed, i),1)
+			line = or(lshift(line,1), bit)
+		}
 		character = or(character, lshift(line,fontwidth*(lineno-1)));
 		lineno = lineno-1;
        	} }
 	/^ENDCHAR/ { print character ".0:\\"; digit = digit+1 }
-	END { print "0.0 )" }
+	END { print indent "x==10?32.0:\\"; print indent "x==11?1792.0:\\"; print indent "0.0 )" }
 '
-
 #STARTCHAR nine
 #ENCODING 57
 #SWIDTH 640 0
