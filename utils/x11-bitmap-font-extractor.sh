44c50ef35d81ac4a9c169004359ef1c12f72d2f9
ludolpif <ludolpif@gmail.com>
2024-06-30 13:28:39 +0200

    x11-bitmap-font-extractor.sh : variable renaming to clarify a bit

--- utils/x11-bitmap-font-extractor.sh
+++ utils/x11-bitmap-font-extractor.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 zcat /usr/share/fonts/X11/misc/4x6.pcf.gz | pcf2bdf | grep -EA13 "^STARTCHAR (zero|one|two|three|four|five|six|seven|eight|nine)$" | awk --non-decimal-data \
 	'BEGIN {
-       		digit=0; fontwidth=4; fontheight=6;
-		indent="        ";
-		print "#define font_width " fontwidth ".0"
-		print "#define font_height " fontheight ".0"
+		digit=0; font_width=4; font_height=6; indent="        ";
+		print "#define font_width " font_width ".0"
+		print "#define font_height " font_height ".0"
 	       	print "#define DigitBin(x) ( \\"
        	}
 	/^STARTCHAR/ { character=0; printf indent "x==" digit "?" }
-	/^BITMAP/ { lineno=fontheight }
-	/^..$/ { if (lineno > 0 ) {
+	/^BITMAP/ { line_number=font_height }
+	/^..$/ { if (line_number > 0 ) {
 		parsed = sprintf("%d", "0x" $1)
 		line = 0
 		for (i=0; i<8; i++) {
 			bit = and(rshift(parsed, i),1)
 			line = or(lshift(line,1), bit)
 		}
-		character = or(character, lshift(line,fontwidth*(lineno-1)));
-		lineno = lineno-1;
+		character = or(character, lshift(line,font_width*(line_number-1)));
+		line_number = line_number-1;
        	} }
 	/^ENDCHAR/ { print character ".0:\\"; digit = digit+1 }
 	END { print indent "x==10?32.0:\\"; print indent "x==11?1792.0:\\"; print indent "0.0 )" }
