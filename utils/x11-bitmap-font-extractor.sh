7aa935cdb0b0c00651b00771f1b185024e6f911e
ludolpif <ludolpif@gmail.com>
2024-06-30 22:37:18 +0200

    x11-bitmap-font-extractor.sh: extend to '.0123456789?-abcdefghijklmnopqrstuvwxyz' characters

--- utils/x11-bitmap-font-extractor.sh
+++ utils/x11-bitmap-font-extractor.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-zcat /usr/share/fonts/X11/misc/4x6.pcf.gz | pcf2bdf | grep -EA13 "^STARTCHAR (zero|one|two|three|four|five|six|seven|eight|nine)$" | awk --non-decimal-data \
+x11_names_regex='period|zero|one|two|three|four|five|six|seven|eight|nine|question|minus|[a-z]'
+ascii_repr='.0123456789?-abcdefghijklmnopqrstuvwxyz'
+
+zcat /usr/share/fonts/X11/misc/4x6.pcf.gz | pcf2bdf | grep -EA13 "^STARTCHAR ($x11_names_regex)$" \
+	| awk --non-decimal-data -v ascii_repr="$ascii_repr" \
 	'BEGIN {
-		digit=0; font_width=4; font_height=6; indent="        ";
+		char_indice=1; font_width=4; font_height=6; indent="        ";
 		print "#define font_width " font_width ".0"
 		print "#define font_height " font_height ".0"
 	       	print "#define DigitBin(x) ( \\"
        	}
-	/^STARTCHAR/ { character=0; printf indent "x==" digit "?" }
+	/^STARTCHAR/ { character=0; printf indent "x=='\''" substr(ascii_repr,char_indice,1) "'\''?" }
 	/^BITMAP/ { line_number=font_height }
 	/^..$/ { if (line_number > 0 ) {
 		parsed = sprintf("%d", "0x" $1)
@@ -18,8 +22,8 @@ zcat /usr/share/fonts/X11/misc/4x6.pcf.gz | pcf2bdf | grep -EA13 "^STARTCHAR (ze
 		character = or(character, lshift(line,font_width*(line_number-1)));
 		line_number = line_number-1;
        	} }
-	/^ENDCHAR/ { print character ".0:\\"; digit = digit+1 }
-	END { print indent "x==10?32.0:\\"; print indent "x==11?1792.0:\\"; print indent "0.0 )" }
+	/^ENDCHAR/ { print character ".0:\\"; char_indice = char_indice+1 }
+	END { print indent "0.0 )" }
 '
 #STARTCHAR nine
 #ENCODING 57
