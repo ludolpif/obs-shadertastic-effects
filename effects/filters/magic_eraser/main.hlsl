ccaf3b99a95465d2e0251191dd20074ea9b8efe6
ludolpif <ludolpif@gmail.com>
2024-07-18 14:26:51 +0200

    Add magic_eraser filter. Need prev_tex in ShaderTastic

---
+++ effects/filters/magic_eraser/main.hlsl
@@ -0,0 +1,75 @@
+// Common parameters for all shaders, as reference. Do not uncomment this (but you can remove it safely).
+/*
+uniform float time;            // Time since the shader is running. Goes from 0 to 1 for transition effects; goes from 0 to infinity for filter effects
+uniform texture2d image;       // Texture of the source (filters only)
+uniform texture2d tex_interm;  // Intermediate texture where the previous step will be rendered (for multistep effects)
+uniform float upixel;          // Width of a pixel in the UV space
+uniform float vpixel;          // Height of a pixel in the UV space
+uniform float rand_seed;       // Seed for random functions
+uniform int current_step;      // index of current step (for multistep effects)
+*/
+// Specific parameters of the shader. They must be defined in the meta.json file next to this one.
+uniform texture2d prev_tex;
+//----------------------------------------------------------------------------------------------------------------------
+
+// These are required objects for the shader to work.
+// You don't need to change anything here, unless you know what you are doing
+sampler_state textureSampler {
+    Filter    = Linear;
+    AddressU  = Clamp;
+    AddressV  = Clamp;
+};
+
+struct VertData {
+    float2 uv  : TEXCOORD0;
+    float4 pos : POSITION;
+};
+
+struct FragData {
+    float2 uv  : TEXCOORD0;
+};
+
+VertData VSDefault(VertData v_in)
+{
+    VertData vert_out;
+    vert_out.uv  = v_in.uv;
+    vert_out.pos = mul(float4(v_in.pos.xyz, 1.0), ViewProj);
+    return vert_out;
+}
+
+float4 EffectLinear(float2 uv)
+{
+    float4 curr = image.Sample(textureSampler, uv);
+    float4 prev = prev_tex.Sample(textureSampler, uv);
+    float4 rgba = curr;
+
+    if ( curr.g > curr.r + curr.b ) {
+        // rgba.a = 0.99;
+        rgba.g = 1.0;
+    }
+    // if ( prev.a < 1.0 ) {
+    if ( prev.g > 0.9 ) {
+        // rgba.a = clamp(prev.a - 0.1, 0.0, 1.0);
+        rgba.g = 1.0;
+        //rgba.a = 0.0;
+    }
+ 
+    return rgba;
+}
+
+// You probably don't want to change anything from this point.
+
+float4 PSEffect(FragData f_in) : TARGET
+{
+    float4 rgba = EffectLinear(f_in.uv);
+    return rgba;
+}
+
+technique Draw
+{
+    pass
+    {
+        vertex_shader = VSDefault(v_in);
+        pixel_shader = PSEffect(f_in);
+    }
+}
