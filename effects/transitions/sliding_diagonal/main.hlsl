cf02b3d5a045f34fae3ca04ad9cc09875b55199b
ludolpif <ludolpif@gmail.com>
2024-07-20 17:09:20 +0200

    Add sliding_diagonal simple transition effect

---
+++ effects/transitions/sliding_diagonal/main.hlsl
@@ -0,0 +1,90 @@
+// Common parameters for all shaders, as reference. Do not uncomment this (but you can remove it safely).
+/*
+uniform float time;            // Time since the shader is running. Goes from 0 to 1 for transition effects; goes from 0 to infinity for filter effects
+uniform texture2d tex_a;       // Texture of the previous frame (transitions only)
+uniform texture2d tex_b;       // Texture of the next frame (transitions only)
+uniform texture2d tex_interm;  // Intermediate texture where the previous step will be rendered (for multistep effects)
+uniform float upixel;          // Width of a pixel in the UV space
+uniform float vpixel;          // Height of a pixel in the UV space
+uniform float rand_seed;       // Seed for random functions
+uniform int current_step;      // index of current step (for multistep effects)
+uniform int nb_steps;          // number of steps (for multisteps effects)
+*/
+
+// Specific parameters of the shader. They must be defined in the meta.json file next to this one.
+//----------------------------------------------------------------------------------------------------------------------
+
+// These are required objects for the shader to work.
+// You don't need to change anything here, unless you know what you are doing
+sampler_state textureSampler {
+    Filter    = Linear;
+    AddressU  = Clamp;
+    AddressV  = Clamp;
+};
+
+struct VertData {
+    float2 uv  : TEXCOORD0;
+    float4 pos : POSITION;
+};
+
+struct FragData {
+    float2 uv  : TEXCOORD0;
+};
+
+VertData VSDefault(VertData v_in)
+{
+    VertData vert_out;
+    vert_out.uv  = v_in.uv;
+    vert_out.pos = mul(float4(v_in.pos.xyz, 1.0), ViewProj);
+    return vert_out;
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+float4 EffectLinear(float2 uv)
+{
+    float2 uv_a = uv; 
+    float2 uv_b = uv; 
+    /*                      negative diagonal term + sliding */
+    uv_a[0] = uv_a[0] + max(0.0, 0.5*(uv_a[1]-1.0) + 1.5*time);
+
+    float4 col_a = tex_a.Sample(textureSampler, uv_a);
+    float4 col_b = tex_b.Sample(textureSampler, uv_b);
+    
+    return (uv_a[0]>1.0)?col_b:col_a;
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+// You probably don't want to change anything from this point.
+
+float4 PSEffect(FragData f_in) : TARGET
+{
+    float4 rgba = EffectLinear(f_in.uv);
+    if (current_step == nb_steps - 1) {
+        rgba.rgb = srgb_nonlinear_to_linear(rgba.rgb);
+    }
+    return rgba;
+}
+
+float4 PSEffectLinear(FragData f_in) : TARGET
+{
+    float4 rgba = EffectLinear(f_in.uv);
+    return rgba;
+}
+
+technique Draw
+{
+    pass
+    {
+        vertex_shader = VSDefault(v_in);
+        pixel_shader = PSEffect(f_in);
+    }
+}
+
+technique DrawLinear
+{
+    pass
+    {
+        vertex_shader = VSDefault(v_in);
+        pixel_shader = PSEffectLinear(f_in);
+    }
+}
