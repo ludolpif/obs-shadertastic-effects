e7711b81392f838a6b2a8b577dc1f30f8926dd05
ludolpif <ludolpif@gmail.com>
2024-08-02 19:23:31 +0200

    debug_values: WIP for v2, working state, before moving huge chunck of text

--- effects/released_filters/debug_values/main-bitwise.hlsl
+++ effects/released_filters/debug_values/main-bitwise.hlsl
@@ -44,158 +44,8 @@ bool insideBox(float2 v, float2 topLeft, float2 bottomRight) {
     Font extracted with : zcat /usr/share/fonts/X11/misc/4x6.pcf.gz | pcf2bdf
     See : https://github.com/ludolpif/obs-shadertastic-effects/blob/main/utils/x11-bitmap-font-extractor.sh
 */
-// int font_width = 4;
-// int font_height = 6;
-float printValue_digitBin(int x) {
-    return (
-        /* . */ x==46?32.0:
-        /* 0 */ x==48?2454816.0:
-        /* 1 */ x==49?2302576.0:
-        /* 2 */ x==50?2441840.0:
-        /* 3 */ x==51?7611440.0:
-        /* 4 */ x==52?5600320.0:
-        /* 5 */ x==53?7418928.0:
-        /* 6 */ x==54?6370592.0:
-        /* 7 */ x==55?7610640.0:
-        /* 8 */ x==56?6628656.0:
-        /* 9 */ x==57?2450480.0:
-        /* ? */ x==63?3416096.0:
-        /* a */ x==97?415072.0:
-        /* b */ x==98?1266992.0:
-        /* c */ x==99?397664.0:
-        /* d */ x==100?4609376.0:
-        /* e */ x==101?152416.0:
-        /* f */ x==102?4354592.0:
-        /* g */ x==103?415299.0:
-        /* h */ x==104?1267024.0:
-        /* i */ x==105?2110064.0:
-        /* j */ x==106?4211779.0:
-        /* k */ x==107?1389904.0:
-        /* l */ x==108?3285616.0:
-        /* m */ x==109?357712.0:
-        /* n */ x==110?218448.0:
-        /* o */ x==111?152864.0:
-        /* p */ x==112?217873.0:
-        /* q */ x==113?415076.0:
-        /* r */ x==114?340240.0:
-        /* s */ x==115?406576.0:
-        /* t */ x==116?2564672.0:
-        /* u */ x==117?349536.0:
-        /* v */ x==118?349472.0:
-        /* w */ x==119?350032.0:
-        /* x */ x==120?336464.0:
-        /* y */ x==121?349763.0:
-        /* z */ x==122?475760.0:
-        /* - */ x==45?28672.0:
-        /* default: ? */ 3416096.0
-    );
-}
-/*
-    Each magic number represents all bits in a grid of size font_width*font_height
-    HLSL functions mostly need floats. IEEE754 single precision floats have 24 bits for mantissa.
-    So, maximum font size is 6x4 with this encoding. For more, use double (51 bits) or a proper texture.
-    The chosen encoding use exponent = 0, so the mantissa and the standard int encoding are the same.
-    Example with '7' : float 7610640.0. Take the int32 encoding of 7610640, as binary :
-    7610640 <=> 0000 0000 0111 0100 0010 0001 0001 0000
-    Each group of font_width bits is a line of the character, keep only the font_height last lines
-    0111
-    0100
-    0010
-    0001
-    0001
-    0000
-    Mirror horizontally
-    1110
-    0010
-    0100
-    1000
-    1000
-    0000
-*/
-
-/**
- * Print a numerical value in the rectangle with it top-right at the coordinates uv
- * @param uv coordinates of the top right pixel of the debug area, in UV coordinates
- * @param value numerical value to print
- * @param nbDecimal number of decimals to print
- * @param fontSize size of the font, 1.0 meaning the full height of the image texture
- * @return true if the pixel located at uv is a debug pixel, false otherwise
- * @example
- * // float2(1.0, 0.0) is top right uv coordinate of the image
- * if (printValue(uv, 42.23, float2(1.0, 0.0), 3, 0.2)) {
- *     // current pixel is a debug one, print it as full red
- *     return float4(1.0, 0.0, 0.0, 1.0);
- * }
- * else {
- *     // actual shader code
- * }
- */
-bool printValue(float2 uv, float value_to_debug, float2 area_topRight, int nbDecimal, float fontSize) {
-    nbDecimal = max(0, nbDecimal);
-    if ((uv.y < 0.0) || (uv.y >= 1.0) || (uv.x < 0.0) || (uv.x >= 1.0)) {
-        return false;
-    }
-    int font_width = 4;
-    int font_height = 6;
-
-    bool isNegative = (value_to_debug < 0.0);
-    bool hasDecimals = (nbDecimal > 0);
-    value_to_debug = abs(value_to_debug);
-    float log10Value = log2(abs(value_to_debug)) / log2(10.0);
-    int biggestIndex = int(max(0.0, floor(max(log10Value, 0.0))));
-
-    float square_height = fontSize / font_height;
-    float square_width = square_height * upixel/vpixel;
-
-    float area_height = font_height * square_height;
-    float area_width = font_width * square_width * (nbDecimal + (hasDecimals ? 1 : 0) + (1 + biggestIndex) + (isNegative ? 1 : 0));
-    float2 area_bottomLeft = area_topRight + float2(-area_width, area_height);
-
-    if (insideBox(uv, area_topRight, area_bottomLeft)) {
-        uv -= float2(area_bottomLeft.x, area_topRight.y);
-        int square_u = int(uv.x / square_width);
-        int square_v = int(uv.y / square_height);
-
-        int digitIndex = square_u / font_width;
-        int digit_u = square_u - digitIndex * font_width;
-
-        float digitBin = 0.0;
-        if (isNegative) {
-            digitIndex--;
-        }
-        if (isNegative && digitIndex == -1) {
-            digitBin = printValue_digitBin(45);
-        }
-        else if (hasDecimals && digitIndex == biggestIndex + 1) {
-            digitBin = printValue_digitBin(46);
-        }
-        else {
-            if (hasDecimals && digitIndex > biggestIndex) {
-                digitIndex--;
-                value_to_debug = fract( value_to_debug );
-            }
-            int currentDigitNegativeIndex = digitIndex-biggestIndex;
-            float currentDigitFloat = fmod(value_to_debug * pow(10.0, currentDigitNegativeIndex), 10.0);
-            int currentDigit;
-            if ( (digitIndex == biggestIndex + nbDecimal) && (currentDigitFloat < 9.0) ) {
-                currentDigit = int(round(currentDigitFloat));
-            }
-            else {
-                currentDigit = int(currentDigitFloat);
-            }
-            digitBin = printValue_digitBin(48+currentDigit);
-        }
-
-        return fmod(digitBin / pow(2.0, (font_height-square_v) * font_width + digit_u), 2.0) >= 1.0;
-    }
-
-    return false;
-}
-
-//FIXME finir le passage de uv à vStringCoords
 float printDCB(float2 vStringCoords, int dcb_digit) {
-    float font_width = 4.0;
-    float font_height = 6.0;
+    float2 glyph_size = float2(4.0,6.0);
     float font[20];
     font[0] = 2454816.0;    /* 0 */
     font[1] = 2302576.0;    /* 1 */
@@ -218,8 +68,10 @@ float printDCB(float2 vStringCoords, int dcb_digit) {
     font[18] = 218448.0;    /* n */
     font[19] = 3416096.0;   /* ? */
 
-    int char_idx = ( dcb_digit >= 0 && dcb_digit < 20)?dcb_digit:19;
-    return floor(mod((font[char_idx] / pow(2.0, floor(fract(vStringCoords.x) * font_width) + (floor((1.0-vStringCoords.y) * font_height) * font_width))), 2.0));
+    float w = glyph_size[0];
+    float h = glyph_size[1];
+    int i = (dcb_digit >= 0 && dcb_digit < 20)?dcb_digit:19;
+    return floor(mod((font[i] / pow(2.0, floor(fract(vStringCoords.x) * w) + (floor((1.0-vStringCoords.y) * h) * w))), 2.0));
 }
 #endif /* _PRINT_VALUE_HLSL */
 
@@ -248,86 +100,78 @@ VertData VSDefault(VertData v_in)
     return vert_out;
 }
 
-int float_decode_pow10_table(int expf) {
-	return
-        expf==9?1000000000:
-        expf==8?100000000:
-        expf==7?10000000:
-        expf==6?1000000:
-        expf==5?100000:
-        expf==4?10000:
-        expf==3?1000:
-        expf==2?100:
-        expf==1?10:
-        expf==0?1:
-		0;
-}
+int2 float_decode_fixed_point_table(int i) {
 
-int2 float_decode_fixed_point_table(float expf) {
-    return
-		expf>0.0?int2(int(pow(2.0,expf)),0): // Not exact if pow > 23 ?
-		/*
-        expf==+29.0?int2(536870912,0):
-        expf==+28.0?int2(268435456,0):
-        expf==+27.0?int2(134217728,0):
-        expf==+26.0?int2(67108864,0):
-        expf==+25.0?int2(33554432,0):
-        expf==+24.0?int2(16777216,0):
-        expf==+23.0?int2(8388608,0):
-        expf==+22.0?int2(4194304,0):
-        expf==+21.0?int2(2097152,0):
-        expf==+20.0?int2(1048576,0):
-        expf==+19.0?int2(524288,0):
-        expf==+18.0?int2(262144,0):
-        expf==+17.0?int2(131072,0):
-        expf==+16.0?int2(65536,0):
-        expf==+15.0?int2(32768,0):
-        expf==+14.0?int2(16384,0):
-        expf==+13.0?int2(8192,0):
-        expf==+12.0?int2(4096,0):
-        expf==+11.0?int2(2048,0):
-        expf==+10.0?int2(1024,0):
-        expf==+ 9.0?int2(512,0):
-        expf==+ 8.0?int2(256,0):
-        expf==+ 7.0?int2(128,0):
-        expf==+ 6.0?int2(64,0):
-        expf==+ 5.0?int2(32,0):
-        expf==+ 4.0?int2(16,0):
-        expf==+ 3.0?int2(8,0):
-        expf==+ 2.0?int2(4,0):
-        expf==+ 1.0?int2(2,0):*/
-        expf==+ 0.0?int2(1,0):
-        expf==- 1.0?int2(0,500000000):
-        expf==- 2.0?int2(0,250000000):
-        expf==- 3.0?int2(0,125000000):
-        expf==- 4.0?int2(0, 62500000):
-        expf==- 5.0?int2(0, 31250000):
-        expf==- 6.0?int2(0, 15625000):
-        expf==- 7.0?int2(0,  7812500):
-        expf==- 8.0?int2(0,  3906250):
-        expf==- 9.0?int2(0,  1953125):
-        expf==-10.0?int2(0,   976562):
-        expf==-11.0?int2(0,   488281):
-        expf==-12.0?int2(0,   244141):
-        expf==-13.0?int2(0,   122070):
-        expf==-14.0?int2(0,    61035):
-        expf==-15.0?int2(0,    30518):
-        expf==-16.0?int2(0,    15259):
-        expf==-17.0?int2(0,     7629):
-        expf==-18.0?int2(0,     3815):
-        expf==-19.0?int2(0,     1907):
-        expf==-20.0?int2(0,      954):
-        expf==-21.0?int2(0,      477):
-        expf==-22.0?int2(0,      238):
-        expf==-23.0?int2(0,      119):
-        expf==-24.0?int2(0,       60):
-        expf==-25.0?int2(0,       30):
-        expf==-26.0?int2(0,       15):
-        expf==-27.0?int2(0,        7):
-        expf==-28.0?int2(0,        4):
-        expf==-29.0?int2(0,        2):
-        expf==-30.0?int2(0,        1):
-        int2(0,0);
+    int pow2_table_pos[30];
+    int pow2_table_neg[31];
+    pow2_table_pos[0]  =         1;
+    pow2_table_pos[1]  =         2;
+    pow2_table_pos[2]  =         4;
+    pow2_table_pos[3]  =         8;
+    pow2_table_pos[4]  =        16;
+    pow2_table_pos[5]  =        32;
+    pow2_table_pos[6]  =        64;
+    pow2_table_pos[7]  =       128;
+    pow2_table_pos[8]  =       256;
+    pow2_table_pos[9]  =       512;
+    pow2_table_pos[10] =      1024;
+    pow2_table_pos[11] =      2048;
+    pow2_table_pos[12] =      4096;
+    pow2_table_pos[13] =      8192;
+    pow2_table_pos[14] =     16384;
+    pow2_table_pos[15] =     32768;
+    pow2_table_pos[16] =     65536;
+    pow2_table_pos[17] =    131072;
+    pow2_table_pos[18] =    262144;
+    pow2_table_pos[19] =    524288;
+    pow2_table_pos[20] =   1048576;
+    pow2_table_pos[21] =   2097152;
+    pow2_table_pos[22] =   4194304;
+    pow2_table_pos[23] =   8388608;
+    pow2_table_pos[24] =  16777216;
+    pow2_table_pos[25] =  33554432;
+    pow2_table_pos[26] =  67108864;
+    pow2_table_pos[27] = 134217728;
+    pow2_table_pos[28] = 268435456;
+    pow2_table_pos[29] = 536870912;
+    pow2_table_neg[1]  = 500000000;
+    pow2_table_neg[2]  = 250000000;
+    pow2_table_neg[3]  = 125000000;
+    pow2_table_neg[4]  =  62500000;
+    pow2_table_neg[5]  =  31250000;
+    pow2_table_neg[6]  =  15625000;
+    pow2_table_neg[7]  =   7812500;
+    pow2_table_neg[8]  =   3906250;
+    pow2_table_neg[9]  =   1953125;
+    pow2_table_neg[10] =    976562;
+    pow2_table_neg[11] =    488281;
+    pow2_table_neg[12] =    244141;
+    pow2_table_neg[13] =    122070;
+    pow2_table_neg[14] =     61035;
+    pow2_table_neg[15] =     30518;
+    pow2_table_neg[16] =     15259;
+    pow2_table_neg[17] =      7629;
+    pow2_table_neg[18] =      3815;
+    pow2_table_neg[19] =      1907;
+    pow2_table_neg[20] =       954;
+    pow2_table_neg[21] =       477;
+    pow2_table_neg[22] =       238;
+    pow2_table_neg[23] =       119;
+    pow2_table_neg[24] =        60;
+    pow2_table_neg[25] =        30;
+    pow2_table_neg[26] =        15;
+    pow2_table_neg[27] =         7;
+    pow2_table_neg[28] =         4;
+    pow2_table_neg[29] =         2;
+    pow2_table_neg[30] =         1;
+
+    if ( i < 30.0 && i >= 0.0 ) {
+        return int2(pow2_table_pos[i],0);
+    }
+    if ( i < 0.0 && i > -31.0 ) {
+        return int2(0,pow2_table_neg[-i]);
+    }
+    return int2(0,0);
 }
 
 // dcb_digit is encoded with some extensions to 4-bits DCB: 0-9:digit, 10:NaN, 11:'.', 12:' ', 13:'-', 14:'e', 15:inf
@@ -352,13 +196,14 @@ int float_decode(in float x, in int wanted_digit, out int exp, out int sign, out
 		dcb_digit = (wanted_digit>5 && wanted_digit<9)?(isnan(x)?10:15):((wanted_digit==4 && sign==-1)?13:12); // for +/-NaN or +/-inf
         return 0;
     }
-    expf = floor(log2(x)); // float exponent without -127 offset
-    exp = int(expf)+127; // IEEE754 encoded 8bits-wide exponent
+    expf = floor(log2(x)); // float exponent without -127 offset (out paraeter)
+    int expi = int(expf); // int exponent without -127 offset (internal use)
+    exp = expi + 127; // IEEE754 encoded 8bits-wide exponent (out parameter)
 
 	// Extract the mantissa bit per bit, compute the fixed_point value simultaneously
 	// using only float values that are exactly represented in IEEE754 encoding (powers of two)
     int extracted_mantissa = 0; // IEEE754 23bits-wide mantissa as int
-    fixed_point = float_decode_fixed_point_table(expf); // Limited range fixed point XXX specifiy the right range
+    fixed_point = float_decode_fixed_point_table(expi); // Limited range fixed point XXX specifiy the right range
     // The 1 is always implicit in the sense that no bit represent it in the mantissa nor exponent bitfields
     float mantissa_implicit_one = pow(2.0, expf);
     for ( float mantissa_pow = expf-1.0; mantissa_pow > expf-24.0; mantissa_pow -= 1.0 ) {
@@ -370,7 +215,7 @@ int float_decode(in float x, in int wanted_digit, out int exp, out int sign, out
             extracted_mantissa += 1;
 			// Fixed point is split into integer part and fractionnal part
 			// The way the floats are encoded garantee there is not carry to handle between those two parts
-            fixed_point += float_decode_fixed_point_table(mantissa_pow);
+            fixed_point += float_decode_fixed_point_table(int(mantissa_pow));
         }
     }
 	// To ease a rudimentary printf("%f",x) function we will return in dcb_digit a decimal digit of rank wanted_digit [0;18[
@@ -385,6 +230,7 @@ int float_decode(in float x, in int wanted_digit, out int exp, out int sign, out
     pow10_table[7] = 10000000;
     pow10_table[8] = 100000000;
     pow10_table[9] = 1000000000;
+
 	if ( wanted_digit < 0 || wanted_digit > 18 ) {
         dcb_digit = 11; // for ' '
     } else if ( wanted_digit == 0 ) {
@@ -392,12 +238,12 @@ int float_decode(in float x, in int wanted_digit, out int exp, out int sign, out
 	} else if ( wanted_digit == 9 ) {
 		dcb_digit = 11; // for '.'
 	} else if ( wanted_digit < 9 ) {
-		int pow10_next = float_decode_pow10_table(9-wanted_digit);
-		int pow10_curr = float_decode_pow10_table(8-wanted_digit);
+		int pow10_next = pow10_table[9-wanted_digit];
+		int pow10_curr = pow10_table[8-wanted_digit];
 		dcb_digit = ( fixed_point[0] % pow10_next ) / pow10_curr;
 	} else {
-		int pow10_next = float_decode_pow10_table(19-wanted_digit);
-		int pow10_curr = float_decode_pow10_table(18-wanted_digit);
+		int pow10_next = pow10_table[19-wanted_digit];
+		int pow10_curr = pow10_table[18-wanted_digit];
 		dcb_digit = ( fixed_point[1] % pow10_next ) / pow10_curr;
 	}
     return extracted_mantissa;
@@ -434,12 +280,6 @@ float4 EffectLinear(float2 uv)
         if ( printDCB(vStringCoords, dcb_digit) == 1.0 ) {
             return color_red;
         }
-    }
-
-    // First example : print a uniform variable with 3 decimals at top right corner of the image
-    // (note : you can't print value that depends on pixel shader's uv)
-    if ( printValue(uv, debug_value, float2(1.0, 0.8), 3, font_size) ) {
-        return color_cyan;
     }
 
     return rgba;
