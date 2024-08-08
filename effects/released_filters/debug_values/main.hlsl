09f90c8cacfe56ebecd105d51c20ab90077d09f8
ludolpif <ludolpif@gmail.com>
2024-08-08 22:48:17 +0200

    debug_values v2, document all functions.

--- effects/released_filters/debug_values/main.hlsl
+++ effects/released_filters/debug_values/main.hlsl
@@ -76,17 +76,46 @@ bool inside_box(float2 v, float2 left_top, float2 right_bottom) {
 #endif /* DEBUG_FONT_GLYPHS */
 /* Note : '+' is 4514880.0 from the x11 font + script, but it's ugly, manually changed here. */
 
-//TODO make all javadoc style explanation of all functions and parameters
+/**
+ * Returns a point in the text_coords space from uv space taking ratio, origin, offset and text height into account.
+ *  text_coords has integer part that represent a glyph, and fractionnal part for glyph pixels.
+ *  text_coords[0] goes from right to left (as numbers digit powers/positions goes)
+ *  text_coords[1] goes from top to bottom (as lines in a terminal)
+ * @param uv point in uv space, should be in [0.0,1.0]²
+ * @param uv_grid_origin point in uv space that becomes the origin [0.0,0.0] in text_coord space
+ * @param uv_aspect_ratio ratio to keep glyph pixels as square in case of non square target texture
+ * @param uv_line_height font size expressed as a fraction of target texture height
+ *  (1.0: one line of text occupies the full texture height. 0.1: ten lines of text avaiable in the texture height)
+ * @param text_offset offset in text_coords space to ease text positionning in a glyph size unit
+ */
 float2 debug_get_text_coords_from_uv(in float2 uv, in float2 uv_grid_origin, in float uv_aspect_ratio, in float uv_line_height, in float2 text_offset) {
     float font_ratio = float(DEBUG_FONT_GLYPH_HEIGHT)/float(DEBUG_FONT_GLYPH_WIDTH);
     return (uv - uv_grid_origin)*float2(-uv_aspect_ratio*font_ratio, 1.0)/uv_line_height - text_offset;
 }
 
-int debug_get_wanted_digit_from_text_coords(float2 text_coords) {
+/**
+ * returns the wanted_digit integer number from a text_coords point for debug_decode_* functions.
+ *  wanted_digit convention for int: 0 will be units, 1 tens, 2 hundreds...
+ *  wanted_digit convention for floats : 0 will be '.', 1 units, 2 tens... -1 first digit of fractionnal part...
+ * @param text_coords the text_coords point from current uv space point (see debug_get_text_coords_from_uv)
+ */
+int debug_get_wanted_digit_from_text_coords(in float2 text_coords) {
     //note: floor() will make a double wanted_digit==0 for text_coords in [-1.0;1.0], round() will not but needs -0.5
     return int(round(text_coords.x-0.5));
 }
 
+/**
+ * returns an rgba pixel value to display the pixel at text_coords of a text grid.
+ *  Meant to help choosing parameters for debug_get_text_coords_from_uv().
+ *  Not really for final render.
+ * @param rgba pixel value to return of the current text_coords is not on the grid
+ * @param text_coords current point in text_coords space
+ * @param text_offset should be same as text_offset in debug_get_text_coords_from_uv() call
+ * @param cols_neg number of columns on the right of (0,0) character
+ * @param lines_neg number of lines over the (0,0) character
+ * @param cols_pos number of columns on the left of (0,0) character
+ * @param lines_pos number of lines below the (0,0) character
+ */
 float4 debug_print_text_grid(in float4 rgba, in float2 text_coords, in float2 text_offset,
         in int cols_neg, in int lines_neg, in int cols_pos, in int lines_pos) {
     // Make lines width thick like 1 pixel in font
@@ -115,7 +144,12 @@ float4 debug_print_text_grid(in float4 rgba, in float2 text_coords, in float2 te
     return float4(0.3, 0.3, 0.3, 1.0);
 }
 
-bool debug_print_glyph(float2 text_coords, int glyph_index) {
+/**
+ * returns true if a debug pixel should be printed on target texture.
+ * @param text_coords the text_coords point from current uv space point (see debug_get_text_coords_from_uv)
+ * @param glyph_index index of the glyph to display from the array made from DEBUG_FONT_GLYPHS
+ */
+bool debug_print_glyph(in float2 text_coords, in int glyph_index) {
 #ifdef _OPENGL
     const float font[24] = float[24](DEBUG_FONT_GLYPHS);
 #else
@@ -131,7 +165,12 @@ bool debug_print_glyph(float2 text_coords, int glyph_index) {
 /* #include "../../shadertastic-lib/debug/decode_int.hlsl" */
 #ifndef _DECODE_INT_HLSL
 #define _DECODE_INT_HLSL
-// To ease a rudimentary printf("%d",x) this function return the character index to display at wanted_digit position
+/**
+ * returns a glyph_index to use with debug_print_glyph() to make a rudimentary printf("%d",int_to_decode), one wanted_digit at a time.
+ * @param int_to_decode int value to be decoded as a decimal number
+ * @param wanted_digit digit number you want to get from int_to_decode (0: units, 1: tens, 2: hundreds...)
+ * @param total_digits int_to_decode will be displayed on total_digits digits + 1 for sign, with leading 0's.
+ */
 int debug_decode_int_decimal_fixed(in int int_to_decode, in int wanted_digit, in int total_digits) {
 #ifdef _OPENGL
     const int pow10_table[10] = int[10](1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000);
@@ -150,13 +189,26 @@ int debug_decode_int_decimal_fixed(in int int_to_decode, in int wanted_digit, in
     return glyph_index;
 }
 
+/**
+ * returns a glyph_index to use with debug_print_glyph() to make a rudimentary printf("%d",int_to_decode), one wanted_digit at a time.
+ *  This is a wrapper for debug_decode_int_decimal_fixed() trying to guess the right total_digits for you.
+ *  for a few int_to_decode values, it may lead to print one spurious leading non-significative 0 
+ *  because it uses approximations (log2(x)/log2(10)) to compute total_digits.
+ * @param int_to_decode int value to be decoded as a decimal number
+ * @param wanted_digit digit number you want to get from int_to_decode (0: units, 1: tens, 2: hundreds...)
+ */
 int debug_decode_int_decimal(in int int_to_decode, in int wanted_digit) {
     // Note: total_digits estimation is not always exact, may a leading 0 could appear
     int total_digits = 1 + int(log2(abs(int_to_decode))/log2(10));
     return debug_decode_int_decimal_fixed(int_to_decode, wanted_digit, total_digits);
 }
 
-// To ease a rudimentary printf("%x",x) this function return the character index to display at wanted_digit position
+/**
+ * returns a glyph_index to use with debug_print_glyph() to make a rudimentary printf("%x",int_to_decode), one wanted_digit at a time.
+ * @param int_to_decode int value to be decoded as an hexadecimal number
+ * @param wanted_digit digit number you want to get from int_to_decode (0: first hex digit, least significate, 1: second hex digit...)
+ * @param total_digits int_to_decode will be displayed on total_digits digits + 2 for "0x" prefix.
+ */
 int debug_decode_int_hexadecimal_fixed(in int int_to_decode, in int wanted_digit, in int total_digits) {
     int glyph_index = 0; // for ' '
     if ( total_digits < 2 ) total_digits=1;
@@ -169,6 +221,12 @@ int debug_decode_int_hexadecimal_fixed(in int int_to_decode, in int wanted_digit
     }
     return glyph_index;
 }
+/**
+ * returns a glyph_index to use with debug_print_glyph() to make a rudimentary printf("%b",int_to_decode), one wanted_digit at a time.
+ * @param int_to_decode int value to be decoded as an binary number
+ * @param wanted_digit digit number you want to get from int_to_decode (0: least significant bit, 1: second bit...)
+ * @param total_digits int_to_decode will be displayed on total_digits digits + 2 for "0b" prefix.
+ */
 int debug_decode_int_binary_fixed(in int int_to_decode, in int wanted_digit, in int total_digits) {
     int glyph_index = 0; // for ' '
     if ( total_digits < 2 ) total_digits=1;
@@ -187,6 +245,11 @@ int debug_decode_int_binary_fixed(in int int_to_decode, in int wanted_digit, in 
 #ifndef _DECODE_FLOAT_HLSL
 #define _DECODE_FLOAT_HLSL
 
+/*
+ * returns the "sign" bitfield as encoded in IEEE754 floats. 0b0: positive numbers, 0b1:negative numbers.
+ *  Internally used by debug_decode_float(). You should not need to call this function directly.
+ * @param float_to_decode float from which we want field extraction
+ */
 int debug_decode_float_sign(in float float_to_decode) {
     return
         float_to_decode < 0.0?1:
@@ -195,17 +258,28 @@ int debug_decode_float_sign(in float float_to_decode) {
         0; // note: -0.0 is not < +0.0 if using comparison operators
 }
 
-/*
- * fixed-point as int2. The first is integer part, the second is fractionnal part.
- *   For negative powers, we use a +9 implied decimal fraction digits.
- *   So, to get the normal representation, divide the table value by 10^9.
+/**
+ * returns a fixed-point decimal reprensentation of a certain mantissa power of two (adjusted by exp)
+ *  Internally used by debug_decode_float(). You should not need to call this function directly.
+ *  The first int is integer part, the second is fractionnal part.
+ *  For fractionnal part, a +9 implied decimal fraction digits is used.
+ *  So, to get the canonical representation, divide the fractionnal part value value by 10^9.
+ * @param mantissa_pow the mantissa power of two (adjusted by exp) to retreive
  */
-int2 debug_decode_float_mantissa_to_fixed_point(float mantissa_pow) {
+int2 debug_decode_float_mantissa_to_fixed_point(in float mantissa_pow) {
     int fpart = int(max(0.0, -mantissa_pow));
     int ipart = int(max(0.0, mantissa_pow));
     return int2( 1 << ipart >> fpart, (1000000000 >> fpart)%1000000000);
 }
 
+/**
+ * returns a glyph_index for a special float value described by glyphs, at wanted_digit.
+ *  Internally used by debug_decode_float(). You should not need to call this function directly.
+ *  It's for +/-inf, +/-nan, +/-0.0.
+ * @param sign Sign of the specal value
+ * @param glyphs triplet of glyph indices needed for the current float special value to display
+ * @param wanted_digit digit number you want to get from glyphs triplet (from 2 to -1 to center nicely, including sign)
+ */
 int debug_format_float_special_values(in int sign, in int3 glyphs, in int wanted_digit) {
     return
         wanted_digit==2?(sign==1?22:1): // for '-' or '+'
@@ -215,6 +289,22 @@ int debug_format_float_special_values(in int sign, in int3 glyphs, in int wanted
         0;
 }
 
+/**
+ * returns a glyph_index to use with debug_print_glyph() to make a rudimentary printf("%f",float_to_decode), one wanted_digit at a time.
+ *  This function has many out paramters to get as many details as possible from float_to_decode.
+ * @param float_to_decode int value to be decoded as a decimal number with fractionnal part
+ * @param wanted_digit digit number you want to get from int_to_decode (1: units, 2: tens, -1: first fractionnal digit...)
+ * @param integer_digits number of digit to display for the integer part of float_to_decode (max 9)
+ * @param fractionnal_digits number of digit to display for the fractionnal part of float_to_decode (max 8)
+ * @param sign outputs the sign bit as encoded in IEE754
+ * @param exp outputs the exp bitfield as encoded in IEE754 (=expf+127 encoded on unsigned int of 8 bits)
+ * @param mant outputs the mantissa bitfield as encoded in IEE754 (23 bits, the 24th bit is always 1 and is implicit)
+ * @param signi outputs the sign as a integer directly usable to multiply numbers against (-1:negative, +1:positive)
+ * @param expf outputs the exponent with IEEE754 offset, as float (but will never have any fractionnal part)
+ * @param fixed_point outputs the fixed point reprensentation of float_to_decode
+ *  (limited with 9 digits for integer part and 8 for fractionnal part)
+ * @param glyph_index outputs the glyph index for in wanted_digit of float_to_decode to use with debug_print_glyph()
+ */
 void debug_decode_float(in float float_to_decode, in int wanted_digit, in int integer_digits, in int fractionnal_digits,
         out int sign, out int exp, out int mant, out int signi,
         out float expf, out int2 fixed_point, out int glyph_index)
